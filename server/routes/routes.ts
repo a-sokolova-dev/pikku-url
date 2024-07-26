3b2dfa0ee616faca05503165f6cd753c7e6c15d7
Anna Sokolova <a.sokolova.dev@gmail.com>
2024-07-26 10:04:44 +0300

    feat: add read API

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -1,9 +1,23 @@
 import { Router } from "express";
 
+import Url from "../models/Url";
+import { generateShortLink } from "../utils";
 import links from "./links";
 
 const router = Router();
 
 router.use("/links", links);
+
+router.get("/:id", async (req, res) => {
+  try {
+    const shortUrl = generateShortLink(req.params.id);
+    const url = await Url.findOne({ shortUrl });
+    if (url) {
+      return res.redirect(url.longUrl);
+    } else res.status(404).json("Not found");
+  } catch (err) {
+    res.status(500).json("Server Error");
+  }
+});
 
 export default router;
