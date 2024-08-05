6b7b1d32901cf99c1428b196f374863458be9775
Anna Sokolova <a.sokolova.dev@gmail.com>
2024-08-05 18:49:36 +0300

    chore: update lint & format scripts to include web

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,14 @@
 import customTsConfig from 'eslint-config/ts'
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [{ ignores: ['web/dist/'] }, ...customTsConfig]
+export default [
+  { ignores: ['web/dist/', 'web/vite.config.ts'] },
+  ...customTsConfig,
+  {
+    files: ['web/**/*.tsx'],
+    rules: {
+      'prefer-const': 'error',
+      'prefer-let/prefer-let': 'off'
+    }
+  }
+]
